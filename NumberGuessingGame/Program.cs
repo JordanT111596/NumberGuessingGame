e8c4e94a5a3cfb26c6db9fef41c83b6901053dfe
JordanT111596 <JordanT111596@gmail.com>
2021-02-27 15:04:53 -0500

    Reached application MVP

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -10,7 +10,44 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number in between 0 and 100: ");
+
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high! Try a lower number!");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low! Try a higher number!");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("That's right! YOU WIN!!!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+
+            } while (win == false);
+
+            Console.WriteLine("Thanks for playing!");
+
+            Console.Write("Press any key to close the application");
+
+            Console.ReadKey(true);
         }
     }
 }
